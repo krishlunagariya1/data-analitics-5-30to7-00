ff379ab813e060cf61843ef7a164f67bf28b128a
krishlunagariya1 <krishlunagariya303@gmail.com>
2026-05-06 23:52:54 +0530

    all data updated

--- module-3-python/core-python/6 strings/1-strings.py
+++ module-3-python/core-python/6 strings/1-strings.py
@@ -1,17 +1,16 @@
 name = "krish"
 
-print(name[0])
-print(name[1])
-print(name[2])
-print(name[3])
-print(name[4])
-print(name[-1])
-print(name[-2])
-print(name[0:3])  # this will print the characters from index 0 to index 2 (3 is not included)
-print(name[1:])  # this will print the characters from index 1 to the end of the string
-print(name[:4])  # this will print the characters from the beginning of the string to
-print(name[-4:])  # this will print the characters from index -4 to the end of the string
-print(name[::2])  # this will print every second character of the string, starting from index 0
-print(name[1::2])  # this will print every second character of the string, starting from index 1
-print(name[::-1])  # this will print the string in reverse order
-
+print(name[0]) #output: k
+print(name[1]) #output: r
+print(name[2]) #output: i
+print(name[3]) #output: s
+print(name[4]) #output: h
+print(name[-1]) #output: h
+print(name[-2]) #output: s
+print(name[0:3]) #output: kri
+print(name[1:]) #output: rish
+print(name[:3]) #output: kri
+print(name[:4]) #output: kris
+print(name[::2]) #output: krs
+print(name[1::2]) #output: rih
+print(name[::-1]) #output: hsi rk
